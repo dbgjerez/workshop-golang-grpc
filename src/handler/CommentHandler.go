c286f74e09843b4ab19168c8764e9bd33fa492fd
David Borrego <dborrego@redhat.com>
2023-02-03 17:10:19 +0100

    read from disc

--- src/handler/CommentHandler.go
+++ src/handler/CommentHandler.go
@@ -5,20 +5,13 @@ import (
 	"context"
 )
 
+var store = domain.NewStore()
+
 type Server struct {
 	domain.UnimplementedCommentServiceServer
 }
 
 func (s *Server) Retrieve(context.Context, *domain.RetrieveRequest) (*domain.Comments, error) {
-	return &domain.Comments{
-		Commets: []*domain.Comment{
-			&domain.Comment{
-				IdComment:  1,
-				IdObject:   12,
-				TypeObject: "film",
-				IdUser:     20,
-				Comment:    "",
-			},
-		},
-	}, nil
+	comments, err := store.ReadStore()
+	return &domain.Comments{Commets: comments}, err
 }
